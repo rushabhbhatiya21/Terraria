20ff957d5522666f5a17ed0531b543c1bbae95ab
rushabhbhatiya21 <bhatiya.rushabh@silvertouch.com>
2026-04-23 17:11:52 +0530

    challenge_4 done, implemented writeEntireFile and readEntireFile (3 overloads)

--- src/gameLayer/saveMap.cpp
+++ src/gameLayer/saveMap.cpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <saveMap.h>
-#include <asserts.h>\
+#include <asserts.h>
 
 struct BlockRepresentation1
 {
@@ -21,6 +21,13 @@ BlockRepresentation1 toBlockRepresentation(Block b)
 	BlockRepresentation1 rez;
 	rez.type = b.type;
 	return rez;
+}
+
+size_t getFileSize(const char* fileName)
+{
+	std::ifstream f(fileName, std::ios::binary | std::ios::ate);
+	if (!f.is_open()) return 0;
+	return static_cast<size_t>(f.tellg());
 }
 
 bool saveBlockDataToFile(std::vector<Block> blocks, int w, int h, const char* fileName)
@@ -118,4 +125,73 @@ bool loadBlockDataFromFile(std::vector<Block>& blocks, int& w, int& h, const cha
 	f.close();
 	return true;
 
+}
+
+bool writeEntireFile(const char* fileName, const void* data, size_t size)
+{
+	std::ofstream f(fileName, std::ios::binary);
+
+	if (!f.is_open()) { return false; }
+
+	f.write((const char*)&VERSION, sizeof(VERSION));
+	f.write((const char*)data, sizeof(size));
+
+	f.close();
+	return true;
+}
+
+bool readEntireFile(const char* fileName, std::vector<unsigned char>& out)
+{
+	std::ifstream f(fileName, std::ios::binary);
+	auto size = getFileSize(fileName);
+	if (size == 0) { return false; }
+
+	int readVersion = 0;
+	f.read((char*)&readVersion, sizeof(readVersion));
+
+	f.seekg(0, std::ios::beg);
+	out.resize(size);
+	if (!f.read((char*)out.data(), size)) { return false; }
+
+	f.close();
+	return true;
+}
+
+bool readEntireFile(const char* fileName, std::string& out)
+{
+	std::ifstream f(fileName, std::ios::binary);
+
+	auto size = getFileSize(fileName);
+	if (size == 0) { return false; }
+
+	int readVersion = 0;
+	f.read((char*)&readVersion, sizeof(readVersion));
+
+	f.seekg(0, std::ios::beg);
+
+	out.resize(size);
+	if (!f.read((char*)out.data(), size)) { return false; }
+
+	f.close();
+	return true;
+}
+
+bool readEntireFile(const char* fileName, void* buffer, size_t bufferSize, size_t& bytesRead)
+{
+	std::ifstream f(fileName, std::ios::binary);
+	auto size = getFileSize(fileName);
+	if (size == 0) { return false; }
+
+	int readVersion = 0;
+	f.read((char*)&readVersion, sizeof(readVersion));
+
+	if (size > bufferSize) { return false; }
+
+	f.seekg(0, std::ios::beg);
+
+	if (!f.read((char*)buffer, size)) { return false; }
+
+	bytesRead = size;
+	f.close();
+	return true;
 }
